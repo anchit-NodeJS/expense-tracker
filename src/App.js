d794ea137809e02f6783a35f12e1864710cf9d94
anchitbhuhan <bhushananchit@gmail.com>
2022-12-19 09:14:22 +0530

    First commit

---
+++ src/App.js
@@ -0,0 +1,39 @@
+import Expenses from "./components/Expenses/Expenses";
+import React from "react";
+
+const App = () => {
+  const items = [
+    {
+      title: "Toilet Paper",
+      amount: 94.12,
+      date: new Date(2020, 7, 14),
+    },
+    { title: "New TV", amount: 799.49, date: new Date(2021, 2, 12) },
+    {
+      title: "Car Insurance",
+      amount: 294.67,
+      date: new Date(2021, 2, 28),
+    },
+    {
+      title: "New Desk (Wooden)",
+      amount: 450,
+      date: new Date(2021, 5, 12),
+    },
+  ];
+
+  // return React.createElement(
+  //   "div",
+  //   {},
+  //   React.createElement("h2", {}, "Let's get started!"),
+  //   React.createElement(Expenses, { items: items })
+  // );
+
+  return (
+    <div>
+      <h2>Let's get started!</h2>
+      <Expenses items={items} />
+    </div>
+  );
+}
+
+export default App;
